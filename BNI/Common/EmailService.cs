8612c0cfb49ac4b4ef045d2cbecef26531be5ef3
Quang Hào <113456985+lequanghao2002@users.noreply.github.com>
2024-04-14 22:47:32 +0700

    reset/change password (#9)

---
+++ BNI/Common/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace BNI.Common
+{
+    public class EmailService
+    {
+        public static void SendMail(string name, string subject, string content, string toMail)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+
+                var smtp = new SmtpClient();
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.Credentials = new NetworkCredential("lequanghaocntt@gmail.com", "sulnllejzzbcvacn");
+                    smtp.Timeout = 20000;
+                }
+
+                MailAddress fromAddress = new MailAddress("lequanghaocntt@gmail.com", name);
+
+                message.From = fromAddress;
+                message.To.Add(toMail);
+                message.Subject = subject;
+                message.IsBodyHtml = true;
+                message.Body = content;
+
+                smtp.Send(message);
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+    }
+}
