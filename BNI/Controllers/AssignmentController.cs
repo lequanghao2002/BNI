9b14a8314ffcc326320f800164bb27b3f52fc86e
YNhi71023 <ynhi71023@gmail.com>
2024-04-24 14:35:58 +0700

    19th Term Members

--- BNI/Controllers/AssignmentController.cs
+++ BNI/Controllers/AssignmentController.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BNI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BNI.Controllers
 {
-    public class Board19Controller : Controller
+    public class AssignmentController : Controller
     {
+        BNIContext _context = new BNIContext();
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var assignment = _context.Assignments.ToList();
+
+            return View(assignment);
         }
     }
 }
