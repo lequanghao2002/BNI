9b2b28a6fbe46076b6cdb8af23d8896539192300
YNhi71023 <ynhi71023@gmail.com>
2024-04-26 11:22:56 +0700

    member logo in home page

---
+++ BNI/Models/ViewModel/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BNI.Models.ViewModel
+{
+    public class HomeViewModel
+    {
+        public List<Post> Blogs { get; set; }
+        public List<Profession> Professions { get; set; }
+        public List<Member> Members { get; set; }
+    }
+}
