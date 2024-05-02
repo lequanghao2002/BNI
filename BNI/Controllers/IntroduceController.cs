661cd7f10fb2ad0cdaa64177a5cc6ca573bbd3b3
phu1110 <phu3365@gmail.com>
2024-05-02 14:10:39 +0700

    introduced

--- BNI/Controllers/IntroduceController.cs
+++ BNI/Controllers/IntroduceController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BNI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BNI.Controllers
 {
     public class IntroduceController : Controller
     {
-        public IActionResult Index()
+        BNIContext _context = new BNIContext();
+        public IntroduceController(BNIContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var introduced = await _context.Assignments.Include(a => a.Term).Include(a => a.Member).ToListAsync();
+            ViewBag.latestTerm = await _context.Terms.OrderByDescending(x => x.StartDate).FirstOrDefaultAsync(); ;
+
+            return View(introduced);
         }
     }
 }
